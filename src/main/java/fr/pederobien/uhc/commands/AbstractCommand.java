b431cc501798f4e47c0a283c59b904d518748dab
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2019-08-24 18:50:39 +0200

    Adding protected configuration

--- src/main/java/fr/pederobien/uhc/commands/AbstractCommand.java
+++ src/main/java/fr/pederobien/uhc/commands/AbstractCommand.java
@@ -4,13 +4,16 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
+import fr.pederobien.uhc.conf.Configuration;
 import fr.pederobien.uhc.conf.Persistence;
 
 public abstract class AbstractCommand implements CommandExecutor {
 	protected Persistence persistence;
+	protected Configuration configuration;
 	
 	public AbstractCommand() {
 		persistence = new Persistence();
+		configuration = Configuration.DEFAULT;
 	}
 
 	public void sendMessageToSender(CommandSender sender, String message) {
