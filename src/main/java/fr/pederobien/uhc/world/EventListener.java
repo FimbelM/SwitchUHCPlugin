2389bc6ed71df3596f30bf1f2d43fabc3220d8f7
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2019-08-24 17:31:09 +0200

    Adding package world in which is a listener that notify the plugin all changes in the world

---
+++ src/main/java/fr/pederobien/uhc/world/EventListener.java
@@ -0,0 +1,61 @@
+package fr.pederobien.uhc.world;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+
+import fr.pederobien.uhc.observer.IObsListener;
+
+public class EventListener implements Listener {
+	private List<IObsListener> observers;
+	
+	public EventListener() {
+		observers = new ArrayList<IObsListener>();
+	}
+
+	@EventHandler(priority=EventPriority.HIGHEST)
+	public void onPlayerJoin(PlayerJoinEvent event) {
+		for (IObsListener obs : observers)
+			obs.onPlayerJoin(event);
+	}
+	
+	@EventHandler(priority=EventPriority.HIGHEST)
+	public void onPlayerDie(PlayerDeathEvent event) {
+		for (IObsListener obs : observers)
+			obs.onPlayerDie(event);
+	}
+	
+	@EventHandler(priority=EventPriority.HIGHEST)
+	public void onPlayerQuit(PlayerQuitEvent event) {
+		for (IObsListener obs : observers)
+			obs.onPlayerQuit(event);
+	}
+	
+	@EventHandler(priority=EventPriority.HIGHEST)
+	public void onPlayerRespawn(PlayerRespawnEvent event) {
+		for (IObsListener obs : observers)
+			obs.onPlayerRespawn(event);
+	}
+	
+	@EventHandler(priority=EventPriority.HIGHEST)
+	public void onPlayerMove(PlayerMoveEvent event) {
+		for (IObsListener obs : observers)
+			obs.onPlayerMove(event);
+	}
+	
+	public void addObservers(IObsListener obs) {
+		observers.add(obs);
+	}
+	
+	public void removeObservers(IObsListener obs) {
+		observers.remove(obs);
+	}
+}
