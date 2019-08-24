b41525a649cedb11e7c879aecba5740838be6e80
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2019-08-24 18:55:51 +0200

    Adding package game that contains all game's commands

---
+++ src/main/java/fr/pederobien/uhc/commands/game/AbstractGameCommand.java
@@ -0,0 +1,7 @@
+package fr.pederobien.uhc.commands.game;
+
+import fr.pederobien.uhc.commands.AbstractCommand;
+
+public abstract class AbstractGameCommand extends AbstractCommand {
+
+}
---
+++ src/main/java/fr/pederobien/uhc/commands/game/StartCommand.java
@@ -0,0 +1,14 @@
+package fr.pederobien.uhc.commands.game;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+public class StartCommand extends AbstractGameCommand {
+
+	@Override
+	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
