331c19c65bca4b6d7846a53465b4c5ae4c021ae9
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2019-08-25 17:58:51 +0200

    AbstractSpawnCommand extends AbstractConfCommand

--- src/main/java/fr/pederobien/uhc/commands/spawn/AbstractSpawnCommand.java
+++ src/main/java/fr/pederobien/uhc/commands/spawn/AbstractSpawnCommand.java
@@ -2,10 +2,10 @@ package fr.pederobien.uhc.commands.spawn;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-import fr.pederobien.uhc.commands.AbstractCommand;
+import fr.pederobien.uhc.commands.configuration.AbstractConfCommand;
 import fr.pederobien.uhc.conf.Spawn;
 
-public abstract class AbstractSpawnCommand extends AbstractCommand {
+public abstract class AbstractSpawnCommand extends AbstractConfCommand {
 	protected static Spawn spawn;
 	
 	public AbstractSpawnCommand(JavaPlugin plugin, String command) {
