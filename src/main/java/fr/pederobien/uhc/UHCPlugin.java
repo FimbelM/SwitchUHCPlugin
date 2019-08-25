f3a9a78a6249032cb0d1526913aabfaf1994c796
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2019-08-25 15:08:23 +0200

    Adding initial set of commands used by the plugin

--- src/main/java/fr/pederobien/uhc/UHCPlugin.java
+++ src/main/java/fr/pederobien/uhc/UHCPlugin.java
@@ -2,11 +2,29 @@ package fr.pederobien.uhc;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import fr.pederobien.uhc.commands.configuration.LoadConfigurationCommand;
+import fr.pederobien.uhc.commands.game.PauseCommand;
+import fr.pederobien.uhc.commands.game.StartCommand;
+import fr.pederobien.uhc.commands.game.StopCommand;
+import fr.pederobien.uhc.commands.spawn.SaveSpawnCommand;
+import fr.pederobien.uhc.commands.team.AddToRandomTeamCommand;
+import fr.pederobien.uhc.commands.team.CreateTeamCommand;
+import fr.pederobien.uhc.commands.team.RemoveAllTeamCommand;
+
 public class UHCPlugin extends JavaPlugin {
 
 	@Override
 	public void onEnable() {
 		getLogger().info("UHC plugin enable");
+		
+		new LoadConfigurationCommand(this, "loadconf");
+		new PauseCommand(this, "pausegame");
+		new StartCommand(this, "startgame");
+		new StopCommand(this, "stopgame");
+		new SaveSpawnCommand(this, "savespawn");
+		new AddToRandomTeamCommand(this, "randomteam");
+		new CreateTeamCommand(this, "teamcreate");
+		new RemoveAllTeamCommand(this, "teamremoveall");
 	}
 
 	@Override
