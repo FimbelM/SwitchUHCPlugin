9884841e68e69296717156ed068fc12bb4c0c86f
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2019-08-26 21:50:16 +0200

    Refactoring code

--- src/main/java/fr/pederobien/uhc/commands/spawn/SaveSpawnCommand.java
+++ src/main/java/fr/pederobien/uhc/commands/spawn/SaveSpawnCommand.java
@@ -1,11 +1,8 @@
 package fr.pederobien.uhc.commands.spawn;
 
-import java.io.IOException;
-
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.xml.sax.SAXException;
 
 public class SaveSpawnCommand extends AbstractSpawnCommand {
 
@@ -20,18 +17,12 @@ public class SaveSpawnCommand extends AbstractSpawnCommand {
 			spawn.setCenter(args[1], args[2], args[3]);
 			spawn.setDimension(args[4], args[5], args[6]);
 			spawn.extract();
-			if (persistence.persisteSpawn(spawn))
-				sendMessageToSender(sender, "Spawn " + spawn.getName() + " saved.");
-			else
-				sendMessageToSender(sender, "This spawn already exist, use command \"updatespawn\"");
+			persistence.persisteSpawn(spawn);
+			sendMessageToSender(sender, "Spawn " + spawn.getName() + " saved.");
 		} catch (IndexOutOfBoundsException e) {
 			sendMessageToSender(sender, "Il n'y a pas assez d'arguments");
 		} catch (NumberFormatException e) {
 			sendMessageToSender(sender, "Impossible de définir le centre du spawn");
-		} catch (SAXException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
 		}
 		return false;
 	}
