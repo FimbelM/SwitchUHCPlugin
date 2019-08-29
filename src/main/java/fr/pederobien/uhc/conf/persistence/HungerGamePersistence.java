5c91ebf0b9dfd03874bf475080b010ff350d77fa
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2019-08-29 21:35:35 +0200

    Implementing the save(Configuration) method

--- src/main/java/fr/pederobien/uhc/conf/persistence/HungerGamePersistence.java
+++ src/main/java/fr/pederobien/uhc/conf/persistence/HungerGamePersistence.java
@@ -7,7 +7,7 @@ public class HungerGamePersistence extends AbstractConfPersistence<HungerGameCon
 	
 	public HungerGamePersistence(HungerGameConfiguration game) {
 		super(game);
-		checkAndWriteDefaultConf(HUNGER_GAME, HungerGameConfiguration.DEFAULT);
+		checkAndWriteDefault(HUNGER_GAME, HungerGameConfiguration.DEFAULT);
 	}
 
 	@Override
@@ -17,6 +17,29 @@ public class HungerGamePersistence extends AbstractConfPersistence<HungerGameCon
 
 	@Override
 	public void save(HungerGameConfiguration configuration) {
+		StringBuilder builder = new StringBuilder();
+		builder.append(openingTag("configuration"))
+		.append(tabAttribut(1, "name", configuration.getName()))
+		.append(tabAttribut(1, "spawn", configuration.getSpawn().getName()))
+		.append(openingTabTag(2, "border"))
+		.append(openingTabTag(1, "border"))
+		.append(openingTabTag(2, "center"))
+		.append(tabAttribut(3, "x", configuration.getBorderCenter().getX()))
+		.append(tabAttribut(3, "y", configuration.getBorderCenter().getY()))
+		.append(tabAttribut(3, "z", configuration.getBorderCenter().getZ()))
+		.append(closingTabTag(2, "center"))
+		.append(openingTabTag(2, "size"))
+		.append(tabAttribut(3, "initial", configuration.getInitialBorderSize()))
+		.append(tabAttribut(3, "final", configuration.getFinalBorderSize()))
+		.append(closingTabTag(2, "size"))
+		.append(closingTabTag(1, "border"))
+		.append(openingTabTag(1, "time"))
+		.append(tabAttribut(3, "total", configuration.getGameTime()))
+		.append(tabAttribut(3, "fraction", configuration.getFractionTime()))
+		.append(tabAttribut(3, "scoreboardrefresh", configuration.getScoreboardRefresh()))
+		.append(closingTabTag(1, "time"))
+		.append(closingTag("configuration"));
 		
+		write(HUNGER_GAME, builder.toString());
 	}
 }
