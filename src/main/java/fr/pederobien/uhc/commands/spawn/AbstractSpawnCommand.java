02f1ead88199bb1fc71c571b6547a363b074147c
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2019-08-29 19:33:20 +0200

    Adding static attribut SpawnPersistence in AbstractSpawnCommand

--- src/main/java/fr/pederobien/uhc/commands/spawn/AbstractSpawnCommand.java
+++ src/main/java/fr/pederobien/uhc/commands/spawn/AbstractSpawnCommand.java
@@ -2,14 +2,16 @@ package fr.pederobien.uhc.commands.spawn;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-import fr.pederobien.uhc.commands.configuration.AbstractConfCommand;
+import fr.pederobien.uhc.commands.AbstractCommand;
 import fr.pederobien.uhc.conf.Spawn;
+import fr.pederobien.uhc.conf.persistence.SpawnPersistence;
 
-public abstract class AbstractSpawnCommand extends AbstractConfCommand {
-	protected static Spawn spawn;
+public abstract class AbstractSpawnCommand extends AbstractCommand {
+	protected static Spawn spawn = new Spawn();
+	protected static SpawnPersistence persistence = new SpawnPersistence(spawn);
+
 	
 	public AbstractSpawnCommand(JavaPlugin plugin, String command) {
 		super(plugin, command);
-		spawn = new Spawn();
 	}
 }
