4f2ba535d6b08338c516060e71d45b09dd1fafe8
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2019-08-30 22:07:26 +0200

    Adding HungerGameEditCommand

---
+++ src/main/java/fr/pederobien/uhc/commands/configuration/hungergame/AbstractHungerGameCommand.java
@@ -0,0 +1,14 @@
+package fr.pederobien.uhc.commands.configuration.hungergame;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+import fr.pederobien.uhc.commands.AbstractCommand;
+import fr.pederobien.uhc.conf.persistence.HungerGamePersistence;
+
+public abstract class AbstractHungerGameCommand extends AbstractCommand {
+	protected HungerGamePersistence hungergamePersistence = new HungerGamePersistence();
+
+	public AbstractHungerGameCommand(JavaPlugin plugin, String command) {
+		super(plugin, command);
+	}
+}
