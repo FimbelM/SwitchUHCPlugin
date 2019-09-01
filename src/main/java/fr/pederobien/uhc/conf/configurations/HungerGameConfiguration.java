0e89388263284da459448cc1aedd51ed66bec199
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2019-09-01 21:55:16 +0200

    Updating default value for hunger game configuration

--- src/main/java/fr/pederobien/uhc/conf/configurations/HungerGameConfiguration.java
+++ src/main/java/fr/pederobien/uhc/conf/configurations/HungerGameConfiguration.java
@@ -6,27 +6,21 @@ import java.util.ArrayList;
 import org.bukkit.block.Block;
 import org.bukkit.scoreboard.Team;
 
-import fr.pederobien.uhc.conf.Spawn;
 import fr.pederobien.uhc.managers.WorldManager;
 
 public class HungerGameConfiguration extends AbstractConfiguration {
 	public static final HungerGameConfiguration DEFAULT = new HungerGameConfiguration("Default configuration");
+	private static final Block DEFAULT_CENTER = WorldManager.getHighestBlockYAt(0, 0);
+	private static final Double DEFAULT_INITIAL_BORDER_SIZE = new Double(2000);
+	private static final Double DEFAULT_FINAL_BORDER_SIZE = new Double(10);
+	private static final LocalTime DEFAULT_GAME_TIME = LocalTime.of(2, 0, 0);
+	private static final LocalTime DEFAULT_FRACTION_TIME = LocalTime.of(0, 45, 0);
+	private static final Long DEFAULT_SCOREBOARD_REFRESH = new Long(5);
 	private Block borderCenter;
-	private double initialBorderSize, finalBorderSize;
+	private Double initialBorderSize, finalBorderSize;
 	private LocalTime gameTime;
 	private LocalTime fractionTime;
-	private long scoreboardRefresh;
-
-	static {
-		DEFAULT.setBorderCenter(WorldManager.getHighestBlockYAt(0, 0));
-		DEFAULT.setInitialBorderSize(2000);
-		DEFAULT.setFinalBorderSize(10);
-		DEFAULT.setFractionTime(LocalTime.of(0, 45, 0));
-		DEFAULT.setGameTime(LocalTime.of(2, 0, 0));
-		DEFAULT.setName("Default configuration");
-		DEFAULT.setSpawn(Spawn.DEFAULT);
-		DEFAULT.setScoreboardRefresh(5);
-	}
+	private Long scoreboardRefresh;
 
 	public HungerGameConfiguration(String name) {
 		setName(name);
@@ -34,19 +28,19 @@ public class HungerGameConfiguration extends AbstractConfiguration {
 	}
 
 	public Block getBorderCenter() {
-		return borderCenter;
+		return borderCenter == null ? DEFAULT_CENTER : borderCenter;
 	}
 
 	public void setBorderCenter(Block borderCenter) {
 		this.borderCenter = borderCenter;
 	}
-	
+
 	public void setBorderCenter(String x, String y, String z) {
 		borderCenter = WorldManager.getBlockAt(Integer.parseInt(x), Integer.parseInt(y), Integer.parseInt(z));
 	}
 
 	public double getInitialBorderSize() {
-		return initialBorderSize;
+		return initialBorderSize == null ? DEFAULT_INITIAL_BORDER_SIZE : initialBorderSize;
 	}
 
 	public void setInitialBorderSize(double initialBorderSize) {
@@ -54,7 +48,7 @@ public class HungerGameConfiguration extends AbstractConfiguration {
 	}
 
 	public double getFinalBorderSize() {
-		return finalBorderSize;
+		return finalBorderSize == null ? DEFAULT_FINAL_BORDER_SIZE : finalBorderSize;
 	}
 
 	public void setFinalBorderSize(double finalBorderSize) {
@@ -62,7 +56,7 @@ public class HungerGameConfiguration extends AbstractConfiguration {
 	}
 
 	public LocalTime getGameTime() {
-		return gameTime;
+		return gameTime == null ? DEFAULT_GAME_TIME : gameTime;
 	}
 
 	public void setGameTime(LocalTime gameTime) {
@@ -70,7 +64,7 @@ public class HungerGameConfiguration extends AbstractConfiguration {
 	}
 
 	public LocalTime getFractionTime() {
-		return fractionTime;
+		return fractionTime == null ? DEFAULT_FRACTION_TIME : fractionTime;
 	}
 
 	public void setFractionTime(LocalTime fractionTime) {
@@ -78,7 +72,7 @@ public class HungerGameConfiguration extends AbstractConfiguration {
 	}
 
 	public long getScoreboardRefresh() {
-		return scoreboardRefresh;
+		return scoreboardRefresh == null ? DEFAULT_SCOREBOARD_REFRESH : scoreboardRefresh;
 	}
 
 	public void setScoreboardRefresh(long scoreboardRefresh) {
