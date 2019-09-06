c46a476427236a3eba752a57321e2b4173456d38
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2019-09-06 23:20:54 +0200

    Implementing load method

--- src/main/java/fr/pederobien/uhc/conf/persistence/SpawnPersistence.java
+++ src/main/java/fr/pederobien/uhc/conf/persistence/SpawnPersistence.java
@@ -1,10 +1,12 @@
 package fr.pederobien.uhc.conf.persistence;
 
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
+import org.w3c.dom.Node;
 
 import fr.pederobien.uhc.conf.Spawn;
 import fr.pederobien.uhc.conf.Spawn.Coordinate;
@@ -25,8 +27,27 @@ public class SpawnPersistence extends AbstractPersistence<Spawn> {
 
 	@Override
 	public void load(String name) throws FileNotFoundException {
-		if (!exist(name))
+		try {
+			Document doc = getDocument(SPAWNS + name + ".xml");
+			Element root = doc.getDocumentElement();
+
+			Node spawnName = root.getElementsByTagName("name").item(0).getChildNodes().item(0);
+			spawn = new Spawn(spawnName.getNodeValue());
+
+			Element center = (Element) root.getElementsByTagName("center").item(0);
+			spawn.setCenter(center.getAttribute("x"), center.getAttribute("y"), center.getAttribute("z"));
+
+			Element blocks = (Element) root.getElementsByTagName("blocks").item(0);
+			List<String> blocksStr = new ArrayList<String>();
+			for (int i = 0; i < blocks.getChildNodes().getLength(); i++) {
+				Element block = (Element) blocks.getChildNodes().item(i);
+				blocksStr.add(block.getAttribute("x") + ";" + block.getAttribute("y") + ";" + block.getAttribute("z")
+						+ ";" + block.getAttribute("material"));
+			}
+			spawn.setBlocks(blocksStr);
+		} catch (NullPointerException e) {
 			throw new FileNotFoundException("Cannot find spawn named " + name);
+		}
 	}
 
 	@Override
@@ -35,17 +56,17 @@ public class SpawnPersistence extends AbstractPersistence<Spawn> {
 		doc.setXmlStandalone(true);
 		Element root = doc.createElement("spawn");
 		doc.appendChild(root);
-		
+
 		Element name = doc.createElement("name");
-		name.setNodeValue(spawn.getName());
+		name.appendChild(doc.createTextNode(spawn.getName()));
 		root.appendChild(name);
-		
+
 		Element center = doc.createElement("center");
 		center.setAttribute("x", "" + spawn.getCenter().getX());
 		center.setAttribute("y", "" + spawn.getCenter().getY());
 		center.setAttribute("z", "" + spawn.getCenter().getZ());
 		root.appendChild(center);
-		
+
 		Element blocks = doc.createElement("blocks");
 		for (Coordinate coord : spawn.getBlocks().keySet()) {
 			Element block = doc.createElement("block");
@@ -56,7 +77,7 @@ public class SpawnPersistence extends AbstractPersistence<Spawn> {
 			blocks.appendChild(block);
 		}
 		root.appendChild(blocks);
-		
+
 		saveDocument(SPAWNS + spawn.getName() + ".xml", doc);
 	}
 
