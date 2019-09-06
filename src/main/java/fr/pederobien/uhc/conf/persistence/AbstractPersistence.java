927ceea558f3b4aa5d94e21efd47937c69c6874e
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2019-09-06 20:00:27 +0200

    Adding methods to save and load xml files

--- src/main/java/fr/pederobien/uhc/conf/persistence/AbstractPersistence.java
+++ src/main/java/fr/pederobien/uhc/conf/persistence/AbstractPersistence.java
@@ -7,10 +7,33 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.Document;
+import org.xml.sax.SAXException;
+
 public abstract class AbstractPersistence<T> implements IPersistence<T> {
 	protected static final String ROOT = "Plugins/UHCPlugin/Ressources/";
 	private BufferedWriter writer;
+	private DocumentBuilder builder;
 	protected boolean saved, loaded;
+
+	public AbstractPersistence() {
+		try {
+			DocumentBuilderFactory f = DocumentBuilderFactory.newInstance();
+			f.setIgnoringElementContentWhitespace(true);
+			builder = f.newDocumentBuilder();
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		}
+	}
 
 	@Override
 	public boolean exist(String name) {
@@ -61,6 +84,34 @@ public abstract class AbstractPersistence<T> implements IPersistence<T> {
 		if (!file.exists()) {
 			file.mkdirs();
 			save();
+		}
+	}
+
+	protected Document newDocument() {
+		return builder.newDocument();
+	}
+
+	protected Document getDocument(String path) {
+		try {
+			return builder.parse(new File(path));
+		} catch (SAXException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	protected void saveDocument(String path, Document doc) {
+		try {
+			TransformerFactory transformerFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformerFactory.newTransformer();
+			DOMSource domSource = new DOMSource(doc);
+			StreamResult streamResult = new StreamResult(new File(path));
+
+			transformer.transform(domSource, streamResult);
+		} catch (TransformerException e) {
+			e.printStackTrace();
 		}
 	}
 
