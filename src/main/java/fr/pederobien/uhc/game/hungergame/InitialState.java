c925b736427861b58d91085d1a7901605385bde2
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2019-09-07 18:01:05 +0200

    The initial state of an hunger game does not override onPlayerJoin and onPlayerRespawn methods

--- src/main/java/fr/pederobien/uhc/game/hungergame/InitialState.java
+++ src/main/java/fr/pederobien/uhc/game/hungergame/InitialState.java
@@ -1,11 +1,5 @@
 package fr.pederobien.uhc.game.hungergame;
 
-import org.bukkit.GameMode;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerRespawnEvent;
-
-import fr.pederobien.uhc.managers.PlayerManager;
-import fr.pederobien.uhc.managers.TeamsManager;
 import fr.pederobien.uhc.managers.WorldManager;
 import fr.pederobien.uhc.task.ScoreboardLauncher;
 import fr.pederobien.uhc.task.TaskLauncher;
@@ -33,17 +27,5 @@ public class InitialState extends AbstractState {
 	@Override
 	public void start() {
 		game.setCurrentState(game.getStart()).start();
-	}
-	
-	@Override
-	public void onPlayerJoin(PlayerJoinEvent event) {
-		PlayerManager.setGameModeOfPlayer(event.getPlayer(), GameMode.ADVENTURE);
-		TeamsManager.teleporte(event.getPlayer().getName(), WorldManager.getSpawnOnJoin());
-	}
-	
-	@Override
-	public void onPlayerRespawn(PlayerRespawnEvent event) {
-		PlayerManager.setGameModeOfPlayer(event.getPlayer(), GameMode.ADVENTURE);
-		TeamsManager.teleporte(event.getPlayer().getName(), WorldManager.getSpawnOnJoin());
 	}
 }
