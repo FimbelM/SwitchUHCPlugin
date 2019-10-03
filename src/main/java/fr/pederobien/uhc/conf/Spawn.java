7b8abd2312085beb17810f5dec3c6af181a9299f
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2019-10-03 12:34:53 +0200

    Fix bug : Save orientation of blocks

--- src/main/java/fr/pederobien/uhc/conf/Spawn.java
+++ src/main/java/fr/pederobien/uhc/conf/Spawn.java
@@ -7,15 +7,17 @@ import java.util.Map;
 
 import org.bukkit.Material;
 import org.bukkit.block.Block;
+import org.bukkit.block.data.BlockData;
 import org.bukkit.util.Vector;
 
+import fr.pederobien.uhc.BukkitManager;
 import fr.pederobien.uhc.managers.WorldManager;
 
 public class Spawn {
 	public static final Spawn DEFAULT;
 	private static final Block DEFAULT_CENTER;
 	private Block center;
-	private HashMap<Coordinate, Material> config;
+	private HashMap<Coordinate, BlockData> config;
 	private int width, height, depth;
 	private String name;
 
@@ -23,12 +25,11 @@ public class Spawn {
 		DEFAULT = new Spawn("DefaultSpawn");
 		DEFAULT_CENTER = WorldManager.getHighestBlockYAt(0, 0);
 		DEFAULT.setDimension(5, 1, 5);
-		
 		DEFAULT.uniformSpawn(Material.BEDROCK);
 	}
 
 	public Spawn(String name) {
-		config = new HashMap<Coordinate, Material>();
+		config = new HashMap<Coordinate, BlockData>();
 		this.name = name;
 	}
 
@@ -42,19 +43,23 @@ public class Spawn {
 
 		for (int x = -width / 2; x < maxWidth; x++)
 			for (int y = 0; y < height; y++)
-				for (int z = -depth / 2; z < maxDepth; z++)
-					config.put(new Coordinate(x, y, z), getBlockFromCenter(x, y, z).getType());
+				for (int z = -depth / 2; z < maxDepth; z++) {
+					Coordinate coord = new Coordinate(x, y, z);
+					config.put(coord, getBlockFromCenter(coord).getBlockData());
+				}
 	}
 
 	public void launch() {
-		for (Coordinate coord : config.keySet())
-			getBlockFromCenter(coord.getX(), coord.getY(), coord.getZ()).setType(config.get(coord));
+		for (Coordinate coord : config.keySet()) {
+			getBlockFromCenter(coord).setType(config.get(coord).getMaterial());
+			getBlockFromCenter(coord).setBlockData(config.get(coord));
+		}
 		WorldManager.setSpawnOnJoin(center.getLocation().clone().add(new Vector(0, 1, 0)));
 	}
 
 	public void remove() {
 		for (Coordinate coord : config.keySet())
-			getBlockFromCenter(coord.getX(), coord.getY(), coord.getZ()).setType(Material.AIR);
+			getBlockFromCenter(coord).setType(Material.AIR);
 	}
 
 	public void setBlocks(List<String> blocks) {
@@ -68,12 +73,10 @@ public class Spawn {
 		int x = Integer.parseInt(info[0]);
 		int y = Integer.parseInt(info[1]);
 		int z = Integer.parseInt(info[2]);
-		Material material = Material.valueOf(info[3]);
-		getBlockFromCenter(x, y, z).setType(material);
-		config.put(new Coordinate(x, y, z), getBlockFromCenter(x, y, z).getType());
+		config.put(new Coordinate(x, y, z), BukkitManager.createBlockData(info[3]));
 	}
 
-	public Map<Coordinate, Material> getBlocks() {
+	public Map<Coordinate, BlockData> getBlocks() {
 		return Collections.unmodifiableMap(config);
 	}
 
@@ -124,21 +127,21 @@ public class Spawn {
 		this.name = name;
 	}
 
-	private Block getBlockFromCenter(int x, int y, int z) {
-		return WorldManager.getBlockAt(getCenter().getX() + x, getCenter().getY() + y, getCenter().getZ() + z);
+	private Block getBlockFromCenter(Coordinate coord) {
+		return getCenter().getRelative(coord.getX(), coord.getY(), coord.getZ());
 	}
-	
+
 	private void uniformSpawn(Material material) {
-		int maxWidth = DEFAULT.width / 2, maxDepth = DEFAULT.depth / 2;
-		if (DEFAULT.width % 2 == 1)
+		int maxWidth = width / 2, maxDepth = depth / 2;
+		if (width % 2 == 1)
 			maxWidth += 1;
-		if (DEFAULT.depth % 2 == 1)
+		if (depth % 2 == 1)
 			maxDepth += 1;
 
-		for (int x = -DEFAULT.width / 2; x < maxWidth; x++)
-			for (int y = 0; y < DEFAULT.height; y++)
-				for (int z = -DEFAULT.depth / 2; z < maxDepth; z++)
-					DEFAULT.config.put(new Coordinate(x, y, z), material);
+		for (int x = -width / 2; x < maxWidth; x++)
+			for (int y = 0; y < height; y++)
+				for (int z = -depth / 2; z < maxDepth; z++)
+					config.put(new Coordinate(x, y, z), material.createBlockData());
 	}
 
 	public class Coordinate {
--- src/main/java/fr/pederobien/uhc/conf/persistence/SpawnPersistence.java
+++ src/main/java/fr/pederobien/uhc/conf/persistence/SpawnPersistence.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bukkit.Material;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -14,7 +15,7 @@ import fr.pederobien.uhc.conf.Spawn.Coordinate;
 
 public class SpawnPersistence extends AbstractPersistence<Spawn> {
 	protected static final String SPAWNS = ROOT + "Spawns/";
-	private static final double CURRENT_VERSION = 1.0;
+	private static final double CURRENT_VERSION = 1.1;
 	private Spawn spawn;
 
 	public SpawnPersistence() {
@@ -39,10 +40,12 @@ public class SpawnPersistence extends AbstractPersistence<Spawn> {
 			case "1.0":
 				load10(root);
 				break;
+			case "1.1":
+				load11(root);
+				break;
 			default:
 				break;
 			}
-
 		} catch (IOException e) {
 			throw new FileNotFoundException("Cannot find spawn named " + name);
 		}
@@ -75,7 +78,7 @@ public class SpawnPersistence extends AbstractPersistence<Spawn> {
 			block.setAttribute("x", "" + coord.getX());
 			block.setAttribute("y", "" + coord.getY());
 			block.setAttribute("z", "" + coord.getZ());
-			block.setAttribute("material", "" + spawn.getBlocks().get(coord));
+			block.setAttribute("blockdata", "" + spawn.getBlocks().get(coord));
 			blocks.appendChild(block);
 		}
 		root.appendChild(blocks);
@@ -103,6 +106,38 @@ public class SpawnPersistence extends AbstractPersistence<Spawn> {
 			if (root.getChildNodes().item(i).getNodeType() != Node.ELEMENT_NODE)
 				continue;
 			Element elt = (Element) root.getChildNodes().item(i);
+			
+			switch (elt.getNodeName()) {
+			case "name":
+				spawn = new Spawn(elt.getChildNodes().item(0).getNodeValue());
+				break;
+			case "center":
+				spawn.setCenter(elt.getAttribute("x"), elt.getAttribute("y"), elt.getAttribute("z"));
+				break;
+			case "blocks":
+				List<String> blocksStr = new ArrayList<String>();
+				for (int j = 0; j < elt.getChildNodes().getLength(); j++) {
+					if (elt.getChildNodes().item(j).getNodeType() != Node.ELEMENT_NODE)
+						continue;
+					Element block = (Element) elt.getChildNodes().item(j);
+					blocksStr.add(block.getAttribute("x") + ";" + block.getAttribute("y") + ";"
+							+ block.getAttribute("z") + ";"
+							+ Material.valueOf(block.getAttribute("material")).createBlockData().getAsString());
+				}
+				spawn.setBlocks(blocksStr);
+				break;
+			default:
+				break;
+			}
+		}
+		System.out.println("spawn loaded");
+	}
+
+	private void load11(Node root) {
+		for (int i = 0; i < root.getChildNodes().getLength(); i++) {
+			if (root.getChildNodes().item(i).getNodeType() != Node.ELEMENT_NODE)
+				continue;
+			Element elt = (Element) root.getChildNodes().item(i);
 
 			switch (elt.getNodeName()) {
 			case "name":
@@ -118,21 +153,23 @@ public class SpawnPersistence extends AbstractPersistence<Spawn> {
 						continue;
 					Element block = (Element) elt.getChildNodes().item(j);
 					blocksStr.add(block.getAttribute("x") + ";" + block.getAttribute("y") + ";"
-							+ block.getAttribute("z") + ";" + block.getAttribute("material"));
-					spawn.setBlocks(blocksStr);
+							+ block.getAttribute("z") + ";" + block.getAttribute("blockdata"));
 				}
+				spawn.setBlocks(blocksStr);
 				break;
 			default:
 				break;
 			}
 		}
 	}
-	
+
 	protected void show() {
 		System.out.println("Name : " + spawn.getName());
-		System.out.println("Center : " + spawn.getCenter().getX() + " " + spawn.getCenter().getY() + " " + spawn.getCenter().getZ());
+		System.out.println("Center : " + spawn.getCenter().getX() + " " + spawn.getCenter().getY() + " "
+				+ spawn.getCenter().getZ());
 		System.out.println("Blocks");
 		for (Coordinate coord : spawn.getBlocks().keySet())
-			System.out.println("\t" + coord.getX() + " " + coord.getY() + " " + coord.getZ() + " " + spawn.getBlocks().get(coord));
+			System.out.println(
+					"\t" + coord.getX() + " " + coord.getY() + " " + coord.getZ() + " " + spawn.getBlocks().get(coord));
 	}
 }
