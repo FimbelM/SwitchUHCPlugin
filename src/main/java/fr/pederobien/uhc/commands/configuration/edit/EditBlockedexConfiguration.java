8ed83bd0cbbe4ddf6d81106cb9091425e1c47acf
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2019-10-07 00:03:29 +0200

    Register all edition arguments in EditBlockedexConfiguration

--- src/main/java/fr/pederobien/uhc/commands/configuration/edit/EditBlockedexConfiguration.java
+++ src/main/java/fr/pederobien/uhc/commands/configuration/edit/EditBlockedexConfiguration.java
@@ -1,15 +1,52 @@
 package fr.pederobien.uhc.commands.configuration.edit;
 
+import fr.pederobien.uhc.commands.configuration.edit.editions.IEdition;
+import fr.pederobien.uhc.commands.configuration.edit.editions.blockedexgame.AsCurrent;
+import fr.pederobien.uhc.commands.configuration.edit.editions.blockedexgame.Current;
+import fr.pederobien.uhc.commands.configuration.edit.editions.blockedexgame.GameTime;
+import fr.pederobien.uhc.commands.configuration.edit.editions.blockedexgame.Help;
+import fr.pederobien.uhc.commands.configuration.edit.editions.blockedexgame.ListConf;
+import fr.pederobien.uhc.commands.configuration.edit.editions.blockedexgame.Load;
+import fr.pederobien.uhc.commands.configuration.edit.editions.blockedexgame.NewConf;
+import fr.pederobien.uhc.commands.configuration.edit.editions.blockedexgame.Rename;
+import fr.pederobien.uhc.commands.configuration.edit.editions.blockedexgame.Save;
+import fr.pederobien.uhc.commands.configuration.edit.editions.blockedexgame.ScoreboardRefresh;
 import fr.pederobien.uhc.conf.IConfigurationContext;
+import fr.pederobien.uhc.conf.persistence.BlockedexPersistence;
 
 public class EditBlockedexConfiguration extends AbstractEditConfiguration {
+	private BlockedexPersistence persistence;
+
+	private IEdition gameTime;
+	private IEdition scoreboardRefresh;
+	private IEdition rename;
+	private IEdition load;
+	private IEdition newConf;
+	private IEdition current;
+	private IEdition asCurrent;
+	private IEdition save;
+	private IEdition list;
+	private IEdition help;
 
 	public EditBlockedexConfiguration(IConfigurationContext context) {
 		super(context);
+
+		persistence = new BlockedexPersistence();
+
+		gameTime = new GameTime(persistence);
+		scoreboardRefresh = new ScoreboardRefresh(persistence);
+		rename = new Rename(persistence);
+		load = new Load(persistence);
+		newConf = new NewConf(persistence);
+		current = new Current(persistence);
+		asCurrent = new AsCurrent(persistence, context);
+		save = new Save(persistence);
+		list = new ListConf(persistence);
+		help = new Help(persistence);
 	}
 
 	@Override
 	protected void setEditions() {
-
+		addToMap(gameTime, scoreboardRefresh, rename, load, newConf, current, asCurrent, save, list, help);
 	}
 }
