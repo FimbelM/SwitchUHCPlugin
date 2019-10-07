b54a6ecaa22a0f7cf912bbee075b0298ce64881d
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2019-10-07 17:28:20 +0200

    Defining the type of game in Blockedex configuration

--- src/main/java/fr/pederobien/uhc/conf/configurations/BlockedexConfiguration.java
+++ src/main/java/fr/pederobien/uhc/conf/configurations/BlockedexConfiguration.java
@@ -4,12 +4,14 @@ import java.util.ArrayList;
 
 import org.bukkit.scoreboard.Team;
 
+import fr.pederobien.uhc.game.blockedexgame.BlockedexGame;
+
 public class BlockedexConfiguration extends AbstractConfiguration {
 	public static final BlockedexConfiguration DEFAULT = new BlockedexConfiguration("DefaultConfiguration");
 	
 	public BlockedexConfiguration(String name) {
 		setName(name);
 		setTeams(new ArrayList<Team>());
-		//setGame(new HungerGame(this));
+		setGame(new BlockedexGame(this));
 	}
 }
