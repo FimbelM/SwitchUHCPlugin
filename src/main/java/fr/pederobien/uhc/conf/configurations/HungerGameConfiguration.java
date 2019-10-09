51115d5b0cdad0d29c3733ff4cb0841a7054c5cd
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2019-10-09 18:35:33 +0200

    HungerGameConfiguration implements IHungerGameConfiguration

--- src/main/java/fr/pederobien/uhc/conf/configurations/HungerGameConfiguration.java
+++ src/main/java/fr/pederobien/uhc/conf/configurations/HungerGameConfiguration.java
@@ -6,10 +6,11 @@ import java.util.ArrayList;
 import org.bukkit.block.Block;
 import org.bukkit.scoreboard.Team;
 
+import fr.pederobien.uhc.conf.configurations.interfaces.IHungerGameConfiguration;
 import fr.pederobien.uhc.game.hungergame.HungerGame;
 import fr.pederobien.uhc.managers.WorldManager;
 
-public class HungerGameConfiguration extends AbstractConfiguration {
+public class HungerGameConfiguration extends AbstractConfiguration implements IHungerGameConfiguration {
 	public static final HungerGameConfiguration DEFAULT = new HungerGameConfiguration("DefaultConfiguration");
 	private static final Block DEFAULT_CENTER = WorldManager.getHighestBlockYAt(0, 0);
 	private static final Double DEFAULT_INITIAL_BORDER_SIZE = new Double(2000);
@@ -25,38 +26,47 @@ public class HungerGameConfiguration extends AbstractConfiguration {
 		setGame(new HungerGame(this));
 	}
 
+	@Override
 	public Block getBorderCenter() {
 		return borderCenter == null ? DEFAULT_CENTER : borderCenter;
 	}
 
+	@Override
 	public void setBorderCenter(Block borderCenter) {
 		this.borderCenter = borderCenter;
 	}
 
+	@Override
 	public void setBorderCenter(String x, String z) {
 		borderCenter = WorldManager.getBelowHighestBlockYAt(Integer.parseInt(x), Integer.parseInt(z));
 	}
 
+	@Override
 	public Double getInitialBorderDiameter() {
 		return initialBorderDiameter == null ? DEFAULT_INITIAL_BORDER_SIZE : initialBorderDiameter;
 	}
 
+	@Override
 	public void setInitialBorderDiameter(double initialBorderDiameter) {
 		this.initialBorderDiameter = initialBorderDiameter;
 	}
 
+	@Override
 	public Double getFinalBorderDiameter() {
 		return finalBorderDiameter == null ? DEFAULT_FINAL_BORDER_SIZE : finalBorderDiameter;
 	}
 
+	@Override
 	public void setFinalBorderDiameter(double finalBorderDiameter) {
 		this.finalBorderDiameter = finalBorderDiameter;
 	}
 
+	@Override
 	public LocalTime getFractionTime() {
 		return fractionTime == null ? DEFAULT_FRACTION_TIME : fractionTime;
 	}
 
+	@Override
 	public void setFractionTime(LocalTime fractionTime) {
 		this.fractionTime = fractionTime;
 	}
