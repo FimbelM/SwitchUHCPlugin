f9f3aa1309d38ec7afceeeb40adc5ce18585d2b1
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2019-10-09 12:31:23 +0200

    Adding property radiusArea to blockedex configuration

--- src/main/java/fr/pederobien/uhc/conf/configurations/BlockedexConfiguration.java
+++ src/main/java/fr/pederobien/uhc/conf/configurations/BlockedexConfiguration.java
@@ -8,10 +8,21 @@ import fr.pederobien.uhc.game.blockedexgame.BlockedexGame;
 
 public class BlockedexConfiguration extends AbstractConfiguration {
 	public static final BlockedexConfiguration DEFAULT = new BlockedexConfiguration("DefaultConfiguration");
+	private static final Integer DEFAULT_RADIUS_AREA_ON_PLAYER_DIE = 5;
+	
+	private Integer radiusArea;
 	
 	public BlockedexConfiguration(String name) {
 		setName(name);
 		setTeams(new ArrayList<Team>());
 		setGame(new BlockedexGame(this));
+	}
+	
+	public Integer getAreaRadiusOnPlayerDie() {
+		return radiusArea == null ? DEFAULT_RADIUS_AREA_ON_PLAYER_DIE : radiusArea;
+	}
+	
+	public void setAreaRadiusOnPlayerDie(int radiusArea) {
+		this.radiusArea = radiusArea;
 	}
 }
--- src/main/java/fr/pederobien/uhc/game/blockedexgame/StartedState.java
+++ src/main/java/fr/pederobien/uhc/game/blockedexgame/StartedState.java
@@ -1,5 +1,11 @@
 package fr.pederobien.uhc.game.blockedexgame;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerRespawnEvent;
+
+import fr.pederobien.uhc.managers.PlayerManager;
+
 public class StartedState extends AbstractBlockedexState {
 
 	public StartedState(IBlockedexGame game) {
@@ -9,5 +15,17 @@ public class StartedState extends AbstractBlockedexState {
 	@Override
 	public void stop() {
 		game.setCurrentState(game.getStop()).stop();
+	}
+	
+	@Override
+	public void onPlayerDie(PlayerDeathEvent event) {
+		for (Player player : PlayerManager.getClosePlayers(event.getEntity(), game.getConfiguration().getAreaRadiusOnPlayerDie()))
+			System.out.println("Decreasing " + player.getName() + "'s health");
+		
+	}
+	
+	@Override
+	public void onPlayerRespawn(PlayerRespawnEvent event) {
+
 	}
 }
