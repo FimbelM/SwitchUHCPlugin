b4214c8f8b157406dace28e9f46580f733c496e5
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2019-10-09 18:34:30 +0200

    IConfiguration implements IUnmodifiableConfiguration

--- src/main/java/fr/pederobien/uhc/conf/configurations/IConfiguration.java
+++ src/main/java/fr/pederobien/uhc/conf/configurations/IConfiguration.java
@@ -5,22 +5,13 @@ import java.util.List;
 
 import org.bukkit.scoreboard.Team;
 
-import fr.pederobien.uhc.conf.IName;
-import fr.pederobien.uhc.game.IGame;
-
-public interface IConfiguration extends IName {
-
-	IGame getGame();
-
-	List<Team> getTeams();
+public interface IConfiguration extends IUnmodifiableConfiguration {
+	
+	void setName(String name);
 
 	void setTeams(List<Team> teams);
 
-	Long getScoreboardRefresh();
-
 	void setScoreboardRefresh(Long refresh);
-
-	LocalTime getGameTime();
 
 	void setGameTime(LocalTime gameTime);
 }
