df1cc4e0d5d3437cc2784864ea8350766290f68a
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2019-10-11 22:35:42 +0200

    Upgrading treatment when player die and respawn

--- src/main/java/fr/pederobien/uhc/game/blockedexgame/StartedState.java
+++ src/main/java/fr/pederobien/uhc/game/blockedexgame/StartedState.java
@@ -2,7 +2,6 @@ package fr.pederobien.uhc.game.blockedexgame;
 
 import java.util.List;
 
-import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
@@ -28,20 +27,28 @@ public class StartedState extends AbstractBlockedexState {
 		event.setKeepInventory(true);
 		Player player = event.getEntity();
 		collegues = TeamsManager.getCollegues(player);
+
+		onPlayerDie(player);
+	}
+
+	@Override
+	public void onPlayerRespawn(PlayerRespawnEvent event) {
+		if (bdPlayerManager.isTeamPlayerEliminated(event.getPlayer()))
+			event.setRespawnLocation(WorldManager.getSpawnOnJoin());
+		else if (collegues.size() > 0)
+			event.setRespawnLocation(TeamsManager.getRandom(collegues).getLocation());
+		else
+			event.setRespawnLocation(WorldManager.getRandomlyLocation(game.getConfiguration().getDiameterAreaOnPlayerRespawn()));
+	}
+
+	private void onPlayerDie(Player player) {
 		if (player.getKiller() instanceof Player) {
 			onPlayerDieByPlayer(player);
 			onPlayerKill(player.getKiller());
 		} else
 			onPlayerDieByEnvironment(player);
-		PlayerManager.setGameModeOfPlayer(player, GameMode.SURVIVAL);
-	}
 
-	@Override
-	public void onPlayerRespawn(PlayerRespawnEvent event) {
-		if (collegues.size() > 0)
-			event.setRespawnLocation(TeamsManager.getRandom(collegues).getLocation());
-		else
-			event.setRespawnLocation(WorldManager.getRandomlyLocation(game.getConfiguration().getDiameterAreaOnPlayerRespawn()));
+		bdPlayerManager.decreasePlayerTeamMaxDeath(player);
 	}
 
 	private void onPlayerDieByEnvironment(Player player) {
@@ -52,8 +59,7 @@ public class StartedState extends AbstractBlockedexState {
 	}
 
 	private void onPlayerDieByPlayer(Player player) {
-		List<Player> players = PlayerManager.getClosePlayers(player,
-				game.getConfiguration().getRadiusAreaOnPlayerDie());
+		List<Player> players = PlayerManager.getCloseCollegues(player, game.getConfiguration().getRadiusAreaOnPlayerDie());
 		if (players.size() > 0) {
 			players.add(player);
 			for (Player pl : players)
@@ -63,8 +69,7 @@ public class StartedState extends AbstractBlockedexState {
 	}
 
 	private void onPlayerKill(Player player) {
-		List<Player> players = PlayerManager.getClosePlayers(player,
-				game.getConfiguration().getRadiusAreaOnPlayerKill());
+		List<Player> players = PlayerManager.getCloseCollegues(player, game.getConfiguration().getRadiusAreaOnPlayerKill());
 		if (players.size() > 0) {
 			players.add(player);
 			for (Player pl : players)
