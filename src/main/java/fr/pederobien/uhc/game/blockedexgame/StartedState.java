a836c90a5b8ddd212efc0e1d49788bacc89bd187
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2019-10-11 13:57:53 +0200

    Implementing onPlayerDie and onPlayerRespawn methods in StartedState for blockedex game

--- src/main/java/fr/pederobien/uhc/game/blockedexgame/StartedState.java
+++ src/main/java/fr/pederobien/uhc/game/blockedexgame/StartedState.java
@@ -2,14 +2,17 @@ package fr.pederobien.uhc.game.blockedexgame;
 
 import java.util.List;
 
+import org.bukkit.GameMode;
 import org.bukkit.entity.Player;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerRespawnEvent;
 
 import fr.pederobien.uhc.managers.PlayerManager;
 import fr.pederobien.uhc.managers.TeamsManager;
+import fr.pederobien.uhc.managers.WorldManager;
 
 public class StartedState extends AbstractBlockedexState {
+	private List<Player> collegues;
 
 	public StartedState(IBlockedexGame game) {
 		super(game);
@@ -22,29 +25,51 @@ public class StartedState extends AbstractBlockedexState {
 
 	@Override
 	public void onPlayerDie(PlayerDeathEvent event) {
+		event.setKeepInventory(true);
 		Player player = event.getEntity();
-		if (player.getKiller() instanceof Player)
+		collegues = TeamsManager.getCollegues(player);
+		if (player.getKiller() instanceof Player) {
 			onPlayerDieByPlayer(player);
-		else
+			onPlayerKill(player.getKiller());
+		} else
 			onPlayerDieByEnvironment(player);
+		PlayerManager.setGameModeOfPlayer(player, GameMode.SURVIVAL);
 	}
 
 	@Override
 	public void onPlayerRespawn(PlayerRespawnEvent event) {
+		if (collegues.size() > 0)
+			event.setRespawnLocation(TeamsManager.getRandom(collegues).getLocation());
+		else
+			event.setRespawnLocation(WorldManager.getRandomlyLocation(game.getConfiguration().getDiameterAreaOnPlayerRespawn()));
+	}
 
-	}
-	
 	private void onPlayerDieByEnvironment(Player player) {
-		for (Player pl : TeamsManager.getCollegues(player))
+		collegues.add(player);
+		for (Player pl : collegues)
 			bdPlayerManager.decreaseMaxHealth(pl, game.getConfiguration().getStepOnMaxHealth());
+		collegues.remove(player);
 	}
-	
+
 	private void onPlayerDieByPlayer(Player player) {
-		List<Player> players = PlayerManager.getClosePlayers(player, game.getConfiguration().getAreaRadiusOnPlayerDie());
-		if (players.size() > 0)
+		List<Player> players = PlayerManager.getClosePlayers(player,
+				game.getConfiguration().getRadiusAreaOnPlayerDie());
+		if (players.size() > 0) {
+			players.add(player);
 			for (Player pl : players)
 				bdPlayerManager.decreaseMaxHealth(pl, game.getConfiguration().getStepOnMaxHealth());
-		else
-			bdPlayerManager.decreaseMaxHealth(player, game.getConfiguration().getStepOnMaxHealth()*2);
+		} else
+			bdPlayerManager.decreaseMaxHealth(player, game.getConfiguration().getStepOnMaxHealth() * 2);
+	}
+
+	private void onPlayerKill(Player player) {
+		List<Player> players = PlayerManager.getClosePlayers(player,
+				game.getConfiguration().getRadiusAreaOnPlayerKill());
+		if (players.size() > 0) {
+			players.add(player);
+			for (Player pl : players)
+				bdPlayerManager.increaseMaxHealth(pl, game.getConfiguration().getStepOnMaxHealth());
+		} else
+			bdPlayerManager.increaseMaxHealth(player, game.getConfiguration().getStepOnMaxHealth() * 2);
 	}
 }
