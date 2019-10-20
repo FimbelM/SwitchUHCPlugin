8088a1d498694f563aac5d7f3384eaec7172dc12
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2019-10-20 22:10:40 +0200

    Adding team list edition

---
+++ src/main/java/fr/pederobien/uhc/commands/configuration/edit/editions/configurations/team/ListTeam.java
@@ -0,0 +1,26 @@
+package fr.pederobien.uhc.commands.configuration.edit.editions.configurations.team;
+
+import java.util.List;
+
+import fr.pederobien.uhc.interfaces.IConfiguration;
+import fr.pederobien.uhc.interfaces.IPersistence;
+
+public class ListTeam<T extends IConfiguration> extends AbstractTeamEditions<T> {
+
+	public ListTeam(IPersistence<T> persistence) {
+		super(persistence, "list", "to list the existing team for a game style");
+	}
+	
+	@Override
+	public String edit(String[] args) {
+		String teams = "";
+		List<String> teamNames = getTeamNamesWithColor();
+		for (String t : teamNames)
+			teams += t + "\r\n";
+		if (teamNames.isEmpty())
+			return "No existing game for " + get().getName() + " style";
+		else if (teamNames.size() == 1)
+			return "Existing team : " + teams;
+		return "Existing teams : " + teams;
+	}
+}
