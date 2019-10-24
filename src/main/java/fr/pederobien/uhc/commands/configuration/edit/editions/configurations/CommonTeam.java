4fa8bf7af9a567848aa0a78835201067d9da0a5b
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2019-10-24 17:40:05 +0200

    Adding TeamEditionsFactory

--- src/main/java/fr/pederobien/uhc/commands/configuration/edit/editions/configurations/CommonTeam.java
+++ src/main/java/fr/pederobien/uhc/commands/configuration/edit/editions/configurations/CommonTeam.java
@@ -1,19 +1,20 @@
 package fr.pederobien.uhc.commands.configuration.edit.editions.configurations;
 
+import fr.pederobien.uhc.commands.configuration.edit.editions.configurations.team.TeamEditionsFactory;
 import fr.pederobien.uhc.interfaces.IConfiguration;
 
 public class CommonTeam<T extends IConfiguration> extends AbstractConfEdition<T> {
-	//private static final TeamEditionsFactory factory = TeamEditionsFactory.getInstance();
+	private final TeamEditionsFactory<T> factory = new TeamEditionsFactory<T>();
 	
 	public CommonTeam() {
 		super("team", "to manage team for game style");
 
-		/*addEdition(factory.createAddPlayerEdition())
+		addEdition(factory.createAddPlayerEdition())
 		.addEdition(factory.createAddTeamEdition())
 		.addEdition(factory.createListTeamEdition())
 		.addEdition(factory.createMovePlayerEdition())
 		.addEdition(factory.createRandomTeamEdition())
 		.addEdition(factory.createRemovePlayerEdition())
-		.addEdition(factory.createRemoveTeamEdition());*/
+		.addEdition(factory.createRemoveTeamEdition());
 	}
 }
---
+++ src/main/java/fr/pederobien/uhc/commands/configuration/edit/editions/configurations/team/TeamEditionsFactory.java
@@ -0,0 +1,43 @@
+package fr.pederobien.uhc.commands.configuration.edit.editions.configurations.team;
+
+import fr.pederobien.uhc.interfaces.IConfiguration;
+import fr.pederobien.uhc.interfaces.IMapEdition;
+
+public class TeamEditionsFactory<T extends IConfiguration> {
+	private boolean alreadyInstanciate = false;
+	
+	public TeamEditionsFactory() {
+		synchronized (TeamEditionsFactory.class) {
+			if (alreadyInstanciate)
+				throw new IllegalArgumentException("Class TeamEditionsFactory already implemented");
+		}
+	}
+
+	public IMapEdition<T> createAddPlayerEdition() {
+		return new AddPlayer<T>();
+	}
+	
+	public IMapEdition<T> createAddTeamEdition() {
+		return new AddTeam<T>();
+	}
+	
+	public IMapEdition<T> createListTeamEdition() {
+		return new ListTeam<T>();
+	}
+	
+	public IMapEdition<T> createMovePlayerEdition() {
+		return new MovePlayer<T>();
+	}
+	
+	public IMapEdition<T> createRandomTeamEdition() {
+		return new RandomTeam<T>();
+	}
+	
+	public IMapEdition<T> createRemovePlayerEdition() {
+		return new RemovePlayer<T>();
+	}
+	
+	public IMapEdition<T> createRemoveTeamEdition() {
+		return new RemoveTeam<T>();
+	}
+}
