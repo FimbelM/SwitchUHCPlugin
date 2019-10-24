c4b2a25c755baa91eba5749d0abea853d532d38d
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2019-10-24 14:26:38 +0200

    Removing useless constructor

--- src/main/java/fr/pederobien/uhc/commands/AbstractCommand.java
+++ src/main/java/fr/pederobien/uhc/commands/AbstractCommand.java
@@ -10,7 +10,6 @@ import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import fr.pederobien.uhc.commands.configuration.edit.editions.IMapEdition;
 import fr.pederobien.uhc.interfaces.IConfigurationContext;
 import fr.pederobien.uhc.world.EventListener;
 
@@ -18,15 +17,10 @@ public abstract class AbstractCommand implements CommandExecutor {
 	protected static IConfigurationContext confContext;
 	protected static EventListener listener;
 
-	protected AbstractCommand(JavaPlugin plugin, String command, IMapEdition edition) {
-		plugin.getCommand(command).setExecutor(this);
-		plugin.getCommand(command).setTabCompleter(edition);
-	}
-
 	protected AbstractCommand(JavaPlugin plugin, String command) {
 		plugin.getCommand(command).setExecutor(this);
 		plugin.getCommand(command).setTabCompleter(new TabCompleter() {
-			
+
 			@Override
 			public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
 				return new ArrayList<String>();
