9a7ff44922213bcc9d3d5efda33eddaa0d0725ba
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2019-10-24 15:53:52 +0200

    Adding base editions to EditBaseConfiguration

--- src/main/java/fr/pederobien/uhc/commands/configuration/edit/EditBaseConfiguration.java
+++ src/main/java/fr/pederobien/uhc/commands/configuration/edit/EditBaseConfiguration.java
@@ -2,6 +2,7 @@ package fr.pederobien.uhc.commands.configuration.edit;
 
 import fr.pederobien.uhc.commands.configuration.edit.editions.bawn.base.BaseEditionsFactory;
 import fr.pederobien.uhc.interfaces.IBase;
+import fr.pederobien.uhc.interfaces.IMapEdition;
 
 public class EditBaseConfiguration extends AbstractEditConfiguration<IBase> {
 	private static final BaseEditionsFactory factory = BaseEditionsFactory.getInstance();
@@ -9,22 +10,23 @@ public class EditBaseConfiguration extends AbstractEditConfiguration<IBase> {
 	public EditBaseConfiguration() {
 		super(factory.getPersistence(), "base", "to configure a base");
 
-		/*IMapEdition launch = factory.createLaunchEdition();
-		IMapEdition newBase = factory.createNewEdition();
+		IMapEdition<IBase> launch = factory.createLaunchEdition();
+		IMapEdition<IBase> newBase = factory.createNewEdition();
 		
-		addEditions(factory.createCenterEdition(),
-				factory.createCurrentEdition(),
-				factory.createDimensionsEdition(),
-				factory.createExtractEdition(),
-				factory.createHelpEdition(this),
-				factory.createListEdition(),
-				newBase, launch,
-				factory.createRenameEdition(),
-				factory.createSaveEdition(),
-				factory.createRemoveEdition());
+		addEdition(factory.createCenterEdition())
+		.addEdition(factory.createCurrentEdition())
+		.addEdition(factory.createDimensionsEdition())
+		.addEdition(factory.createExtractEdition())
+		.addEdition(factory.createHelpEdition(this))
+		.addEdition(launch)
+		.addEdition(factory.createListEdition())
+		.addEdition(newBase)
+		.addEdition(factory.createRemoveEdition())
+		.addEdition(factory.createRenameEdition())
+		.addEdition(factory.createSaveEdition());
 		
 		getEditions().get(launch.getLabel()).setAvailable(true);
-		getEditions().get(newBase.getLabel()).setAvailable(true);*/
+		getEditions().get(newBase.getLabel()).setAvailable(true);
 	}
 	
 	/*@Override
