8cd0e84518d514fd051bc787063de7428b47b286
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2019-10-25 15:42:38 +0200

    Removing already extended interface

--- src/main/java/fr/pederobien/uhc/interfaces/IEditConfiguration.java
+++ src/main/java/fr/pederobien/uhc/interfaces/IEditConfiguration.java
@@ -3,5 +3,5 @@ package fr.pederobien.uhc.interfaces;
 import fr.pederobien.uhc.observer.IObsPersistence;
 
 public interface IEditConfiguration<T extends IUnmodifiableName>
-		extends IEdition, IPersistenceEdition<T>, IWithChildEdition<T>, IObsPersistence<T> {
+		extends IPersistenceEdition<T>, IWithChildEdition<T>, IObsPersistence<T> {
 }
