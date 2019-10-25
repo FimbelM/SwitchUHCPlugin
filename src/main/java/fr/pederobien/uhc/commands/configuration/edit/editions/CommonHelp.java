38c855dea8ac4e6d2daf5c760e4c91c88ceffcea
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2019-10-25 12:40:16 +0200

    Upgrading completion for edition commands help

--- src/main/java/fr/pederobien/uhc/commands/configuration/edit/editions/CommonHelp.java
+++ src/main/java/fr/pederobien/uhc/commands/configuration/edit/editions/CommonHelp.java
@@ -1,5 +1,11 @@
 package fr.pederobien.uhc.commands.configuration.edit.editions;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
 import fr.pederobien.uhc.interfaces.IEditConfiguration;
 import fr.pederobien.uhc.interfaces.IMapEdition;
 import fr.pederobien.uhc.interfaces.IUnmodifiableName;
@@ -24,5 +30,18 @@ public class CommonHelp<T extends IUnmodifiableName> extends AbstractMapEdition<
 
 		}
 		return conf.help();
+	}
+
+	@Override
+	public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+		try {
+			IMapEdition<T> edition = conf.getEditions().get(args[0]);
+			for (int i = 1; i < args.length; i++) {
+				edition = edition.getEditions().get(args[i]);
+			}
+			return filter(edition.getEditions().values().stream().filter(e -> e.isAvailable()).map(e -> e.getLabel()).collect(Collectors.toList()), args[args.length - 1]);
+		} catch (IndexOutOfBoundsException | NullPointerException e) {
+			return emptyList();
+		}
 	}
 }
