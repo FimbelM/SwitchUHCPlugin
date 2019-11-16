60bfc72a6814bca522131f20c61e91e204f24062
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2019-11-16 11:54:06 +0100

    Updating default hunger game configuration

--- src/main/java/fr/pederobien/uhc/persistence/loaders/configurations/hungergame/HungerGameDefaultContent.java
+++ src/main/java/fr/pederobien/uhc/persistence/loaders/configurations/hungergame/HungerGameDefaultContent.java
@@ -11,12 +11,21 @@ public class HungerGameDefaultContent implements IDefaultContent {
 
 	@Override
 	public String getDefaultXmlContent() {
-		return "<?xml version=\"1.0\" encoding=\"UTF-8\"?><hungergame>\r\n" + "  <version>1.1</version>\r\n"
-				+ "  <name>" + getName() + "</name>\r\n" + "  <border>\r\n" + "    <center x=\"0\" z=\"0\"/>\r\n"
-				+ "    <diameter final=\"10.0\" initial=\"2000.0\"/>\r\n" + "  </border>\r\n"
-				+ "  <time fraction=\"00:45\" game=\"02:00\" scoreboardrefresh=\"5\"/>\r\n" + "  <teams>\r\n"
-				+ "    <team color=\"dark_aqua\" name=\"chevalier\"/>\r\n"
-				+ "    <team color=\"dark_red\" name=\"barbare\"/>\r\n" + "  </teams>\r\n" + "</hungergame>";
+		return "<?xml version=\"1.0\" encoding=\"UTF-8\"?><hungergame>\r\n" + 
+				"  <version>1.2</version>\r\n" + 
+				"  <name>DefaultHungerGameStyle</name>\r\n" + 
+				"  <border>\r\n" + 
+				"    <center x=\"0\" z=\"0\"/>\r\n" + 
+				"    <diameter final=\"10.0\" initial=\"2000.0\"/>\r\n" + 
+				"  </border>\r\n" + 
+				"  <time fraction=\"00:45\" game=\"02:00\" pvp=\"00:00\" scoreboardrefresh=\"5\" warning=\"00:05\"/>\r\n" + 
+				"  <teams>\r\n" + 
+				"    <team color=\"dark_aqua\" name=\"knights\"/>\r\n" + 
+				"    <team color=\"yellow\" name=\"vikings\"/>\r\n" + 
+				"    <team color=\"dark_red\" name=\"barbaric\"/>\r\n" + 
+				"    <team color=\"gold\" name=\"spartiates\"/>\r\n" + 
+				"  </teams>\r\n" + 
+				"</hungergame>";
 	}
 
 }
