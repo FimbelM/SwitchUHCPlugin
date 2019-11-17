40984b78facc4780ea9085181ff41e8f9122240e
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2019-11-17 10:09:47 +0100

    fix team's bug for all configurations

--- src/main/java/fr/pederobien/uhc/persistence/AbstractConfPersistence.java
+++ src/main/java/fr/pederobien/uhc/persistence/AbstractConfPersistence.java
@@ -1,11 +1,20 @@
 package fr.pederobien.uhc.persistence;
 
+import java.io.FileNotFoundException;
+
 import fr.pederobien.uhc.interfaces.IConfiguration;
+import fr.pederobien.uhc.interfaces.IPersistence;
 
 public abstract class AbstractConfPersistence<T extends IConfiguration> extends AbstractPersistence<T> {
 	protected static final String GAME = ROOT + "Games/";
 
 	public AbstractConfPersistence(IDefaultContent defaultContent) {
 		super(defaultContent);
+	}
+	
+	@Override
+	public IPersistence<T> load(String name) throws FileNotFoundException {
+		get().getTeams().clear();
+		return super.load(name);
 	}
 }
