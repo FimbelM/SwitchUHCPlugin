b1793927ffc9e65782d55cc2e67def9b2a946915
Martin <lpourtou2@gmail.com>
2020-04-04 18:56:41 +0200

    Updating player location scoreboard for nether location

--- src/main/java/fr/martinfimbel/switchuhc/helpers/PlayerHelper.java
+++ src/main/java/fr/martinfimbel/switchuhc/helpers/PlayerHelper.java
@@ -4,18 +4,27 @@ import org.bukkit.Location;
 import org.bukkit.entity.Player;
 
 import fr.martinfimbel.switchuhc.interfaces.IUnmodifiableBorderConfiguration;
+import fr.martinfimbel.switchuhc.managers.WorldManager;
 
 public class PlayerHelper {
 	private static IUnmodifiableBorderConfiguration configuration;
 
 	public static Location getPlayerLocationRelativeToBorderCenter(Player player) {
-		if(configuration == null)
+		if (configuration == null)
 			return player.getLocation();
 		Location playerloc = player.getLocation(), centerLocation = configuration.getBorderCenter().getLocation();
-		Location locationToBorderCenter = new Location(player.getWorld(),
-				playerloc.getBlockX() - centerLocation.getBlockX(), playerloc.getBlockY(),
-				(playerloc.getBlockZ() - centerLocation.getBlockZ()));
-		return locationToBorderCenter;
+				int netherCenterLocationX = centerLocation.getBlockX()/8;
+				int netherCenterLocationZ = centerLocation.getBlockZ()/8;
+		if (player.getWorld() == WorldManager.SURFACE_WORLD) {
+			Location locationToBorderCenter = new Location(player.getWorld(),
+					playerloc.getBlockX() - centerLocation.getBlockX(), playerloc.getBlockY(),
+					(playerloc.getBlockZ() - centerLocation.getBlockZ()));
+			return locationToBorderCenter;
+		} else {
+			Location locationToBorderCenter = new Location(player.getWorld(), playerloc.getBlockX() - netherCenterLocationX, playerloc.getBlockY(),
+					(playerloc.getBlockZ() - netherCenterLocationZ));
+			return locationToBorderCenter;
+		}
 	}
 
 	public static void setCurrentConfiguration(IUnmodifiableBorderConfiguration configuration) {
